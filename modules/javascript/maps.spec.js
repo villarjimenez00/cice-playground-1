7525925db4aa14874691a32796e6479d8b4efa64
Alejandro Villar Jimenez <villarjimenez00@gmail.com>
2020-02-18 12:00:14 +0100

    ejercicos javascript - map completado - arrays en proceso

--- modules/javascript/maps.spec.js
+++ modules/javascript/maps.spec.js
@@ -1,19 +1,81 @@
 describe('map', () => {
-  it('should set a value with key foo and value bar', () => {})
+  it('should set a value with key foo and value bar', () => {
+    const map = new Map()
 
-  it('should set some value and get it', () => {})
+    map.set('foo', 'bar')
 
-  it('should tell me the size', () => {})
+    expect(map.size).toBe(1)
+  })
 
-  it('should check that it has some value', () => {})
+  it('should set some value and get it', () => {
+    const map = new Map()
 
-  it('should check that it has some value after deleting a value', () => {})
+    map.set('foo', 'bar')
+    const actual = map.get('foo')
 
-  it('should delete all values', () => {})
+    expect(actual).toBe('bar')
+  })
 
-  it('should execute a callback for each value', () => {})
+  it('should tell me the size', () => {
+    const map = new Map([
+      ['foo', 'bar'],
+      ['key', 'value']
+    ])
 
-  it('should be transformed to an array', () => {})
+    expect(map.size).toBe(2)
+  })
+
+  it('should check that it has some value', () => {
+    const map = new Map([
+      ['foo', 'bar'],
+      ['key', 'value']
+    ])
+
+    expect(map.has('buzz')).toBe(false)
+  })
+
+  it('should check that it has some value after deleting a value', () => {
+    const map = new Map([
+      ['foo', 'bar'],
+      ['key', 'value']
+    ])
+
+    map.delete('key')
+
+    expect(map.has('key')).toBe(false)
+  })
+
+  it('should delete all values', () => {
+    const map = new Map([
+      ['foo', 'bar'],
+      ['key', 'value']
+    ])
+
+    map.clear()
+
+    expect(map.size).toEqual(0)
+  })
+
+  it('should execute a callback for each value', () => {
+    const myMap = new Map([
+      ['foo', 'bar'],
+      ['key', 'value']
+    ])
+    const callback = jest.fn()
+
+    myMap.forEach(callback)
+
+    expect(callback).toBeCalledTimes(2)
+  })
+
+  it('should be transformed to an array', () => {
+    const map = new Map([
+      ['foo', 'bar'],
+      ['key', 'value']
+    ])
+    const actual = Array.from(map)
+    expect(actual).toBeInstanceOf(Array)
+  })
 
   it('should be iterated with a for loop', () => {})
 })
