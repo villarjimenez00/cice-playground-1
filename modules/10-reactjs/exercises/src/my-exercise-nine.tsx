47c6f9b5433c48cb4109379559974db8ac58fef4
Alejandro Villar Jimenez <villarjimenez00@gmail.com>
2020-04-14 18:38:50 +0200

    ejercicios repaso react

--- modules/10-reactjs/exercises/src/my-exercise-nine.tsx
+++ modules/10-reactjs/exercises/src/my-exercise-nine.tsx
@@ -1,21 +1,17 @@
 import React, { useState, useEffect } from 'react'
 
 const useWindowData = () => {
-  const [width, setWidth] = useState(0)
-  const [height, setHeight] = useState(0)
-
-  const [state, setState] = useState(0)
-
-  const getSize = () => {
-    setWidth(window.innerWidth)
-    setHeight(window.innerHeight)
-  }
-  window.addEventListener('resize', () => setState(state + 1))
+  const [width, setWidth] = useState(window.innerWidth)
+  const [height, setHeight] = useState(window.innerHeight)
 
   useEffect(() => {
-    getSize()
-    return () => window.removeEventListener('keyup', () => setState(state - 1))
-  }, [state])
+    const getSize = () => {
+      setWidth(window.innerWidth)
+      setHeight(window.innerHeight)
+    }
+    window.addEventListener('resize', getSize)
+    return () => window.removeEventListener('resize', getSize)
+  })
 
   return { width, height }
 }
--- modules/10-reactjs/exercises/src/my-exercise-ten.tsx
+++ modules/10-reactjs/exercises/src/my-exercise-ten.tsx
@@ -3,29 +3,29 @@ import React, { useState, useEffect } from 'react'
 const usePressKeys = () => {
   const [keys, setKeys] = useState<string[]>([])
 
-  const getKeys = () => {
-    document.addEventListener('keypress', (e: KeyboardEvent) => setKeys([...keys, e.code]))
+  const getKeys = (e: KeyboardEvent) => {
+    setKeys([...keys, e.code])
   }
 
   useEffect(() => {
-    getKeys()
-    document.removeEventListener('keypress', () => console.log('remove event listener'))
+    document.addEventListener('keydown', getKeys)
+    return () => document.removeEventListener('keydown', getKeys)
   })
+
   return { keys }
 }
 
 export const MyExerciseTen: React.FC = () => {
   const { keys } = usePressKeys()
 
-  const id = Math.random()
   return (
     <>
       <h1>Exercise ten</h1>
       <hr />
       <h4>Pressed Keys</h4>
       <ul>
-        {keys.map(item => (
-          <li key={item + id}>{item + id}</li>
+        {keys.map((item, index) => (
+          <li key={index}>{item}</li>
         ))}
       </ul>
     </>
