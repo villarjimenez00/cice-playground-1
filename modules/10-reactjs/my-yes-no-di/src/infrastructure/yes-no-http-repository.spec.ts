da385cb1a855c3240233182141d282396412e526
Alejandro Villar Jimenez <villarjimenez00@gmail.com>
2020-04-20 00:44:24 +0200

    clase test ts-mockito

--- modules/10-reactjs/my-yes-no-di/src/infrastructure/yes-no-http-repository.spec.ts
+++ modules/10-reactjs/my-yes-no-di/src/infrastructure/yes-no-http-repository.spec.ts
@@ -1,5 +1,44 @@
 import { YesNoHttpRepository } from './yes-no-http-repository'
+import { Http } from './http'
+import { mock, when, instance, anyString } from 'ts-mockito'
+import { YesNoDtoToYesNoMapper } from './yes-no-dto-to-yes-no-mapper'
+import { YesNoDto } from './yes-no-dto'
+import { verify } from 'crypto'
+import { YesNo } from '../domain/yes-no'
 
-describe('http repository', () => {
-  it('should return YesNo mapped result', () => {})
+describe('yesno http repository', () => {
+  it('should call http', async () => {
+    const http: Http = mock(Http)
+    const yesNoDtoToYesNoMapper: YesNoDtoToYesNoMapper = mock(YesNoDtoToYesNoMapper)
+    const yesNoHttpRepository = new YesNoHttpRepository(
+      instance(http),
+      instance(yesNoDtoToYesNoMapper)
+    )
+
+    await yesNoHttpRepository.find()
+
+    verify(http.get(anyString())).called()
+  })
+
+  it('should return YesNo mapped result', async () => {
+    const http: Http = mock(Http)
+    const yesNoDtoToYesNoMapper: YesNoDtoToYesNoMapper = mock(YesNoDtoToYesNoMapper)
+    const yesNoDto: YesNoDto = {
+      answer: 'yes',
+      forced: false,
+      image: 'img_url',
+    }
+    const yesNo: YesNo = { answer: 'yes', image: 'imageUrl' }
+    when(http.get(anyString())).thenResolve(yesNoDto)
+    when(yesNoDtoToYesNoMapper.map(yesNoDto)).thenReturn(yesNo)
+
+    const yesNoHttpRepository = new YesNoHttpRepository(
+      instance(http),
+      instance(yesNoDtoToYesNoMapper)
+    )
+
+    const actual = await yesNoHttpRepository.find()
+
+    expect(actual).toBe(yesNo)
+  })
 })
